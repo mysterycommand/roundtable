0ba89eb45a0a49d568572e30aeb0348a396f8a75
Matt Hayes <matt@mysterycommand.com>
2021-02-15 18:41:17 -0600

    refactor: this works, I don't *love* the semantics yet, but it works

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -3,12 +3,20 @@ import { createServer } from 'http';
 import evt from 'evt';
 import ws from 'ws';
 
+/**
+ * need the `*.js` extension here
+ * @see https://github.com/nodejs/node/issues/32103#issuecomment-595806356
+ */
 import { createPeerConnection } from './lib/createPeerConnection.js';
 import { createSocketServer } from './lib/createSocketServer.js';
 
 const { Evt } = evt;
 
-const server = createServer();
+const server = createServer(/* (req, res) => {
+  console.log(req.url);
+  res.writeHead(200);
+  res.end('<!doctype html><meta charset=utf-8>', 'utf-8');
+} */);
 const socketServer = createSocketServer({ server });
 const connections: Map<ws, RTCPeerConnection> = new Map();
 
