730e5894699ea423f33965075d375aa71abb8a09
Matt Hayes <matt@mysterycommand.com>
2021-02-17 23:16:09 -0600

    feat: kind of a big commit, but I can't think of a way to partially do this and know it's working

--- apps/client/src/index.ts
+++ apps/client/src/index.ts
@@ -2,12 +2,21 @@ import evt from 'evt';
 
 import { el, rtcp, ws } from './lib/helpers.js';
 
-const { Evt } = evt;
+/**
+ * @see https://github.com/microsoft/TypeScript-DOM-lib-generator/pull/966
+ */
+declare global {
+  interface RTCPeerConnection {
+    setLocalDescription(description?: RTCSessionDescriptionInit): Promise<void>;
+  }
+}
+
 const {
   devicePixelRatio: dpr,
   // requestAnimationFrame: raf,
   // cancelAnimationFrame: caf,
 } = window;
+const { Evt } = evt;
 
 const canvas = el<HTMLCanvasElement>('canvas');
 if (!canvas) {
@@ -22,6 +31,80 @@ if (!context) {
 const socket = ws('ws://localhost:8080');
 const connection = rtcp();
 const channel = connection.createDataChannel('@rnd/state');
+
+Evt.merge([
+  Evt.from<Event>(connection, 'connectionstatechange'),
+  Evt.from<Event>(connection, 'signalingstatechange'),
+]).attach(() => {
+  console.log('connection.connectionState', connection.connectionState);
+  console.log('connection.signalingState', connection.signalingState);
+});
+
+const isMessageEvent = (
+  event: Event | RTCErrorEvent | MessageEvent,
+): event is MessageEvent => event.type === 'message';
+
+Evt.merge([
+  Evt.from<Event>(channel, 'open'),
+  Evt.from<Event>(channel, 'close'),
+  Evt.from<RTCErrorEvent>(channel, 'error'),
+  Evt.from<MessageEvent<string>>(channel, 'message'),
+]).attach((event) => {
+  console.log('event.type', event.type);
+
+  if (isMessageEvent(event) && event.data) {
+    console.log('event.data', event.data);
+  }
+
+  console.log('channel.bufferedAmount', channel.bufferedAmount);
+  console.log('channel.id', channel.id);
+  console.log('channel.label', channel.label);
+  console.log('channel.maxPacketLifeTime', channel.maxPacketLifeTime);
+  console.log('channel.maxRetransmits', channel.maxRetransmits);
+  console.log('channel.negotiated', channel.negotiated);
+  console.log('channel.ordered', channel.ordered);
+  console.log('channel.priority', channel.priority);
+  console.log('channel.protocol', channel.protocol);
+  console.log('channel.readyState', channel.readyState);
+});
+
+/**
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/WebRTC_API/Perfect_negotiation#Handling_the_negotiationneeded_event
+ */
+Evt.from<Event>(connection, 'negotiationneeded').attach(async () => {
+  try {
+    await connection.setLocalDescription();
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+/**
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/WebRTC_API/Perfect_negotiation#Handling_incoming_ICE_candidates
+ */
+Evt.from<RTCPeerConnectionIceEvent>(connection, 'icecandidate').attach(
+  ({ candidate }) => {
+    socket.send(JSON.stringify({ candidate }));
+  },
+);
+
+/**
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/WebRTC_API/Perfect_negotiation#Handling_incoming_messages_on_the_signaling_channel
+ */
+Evt.from<MessageEvent<string>>(socket, 'message').attach(async ({ data }) => {
+  const { candidate, description } = JSON.parse(data);
+  console.log({ candidate, description });
+
+  try {
+    candidate && (await connection.addIceCandidate(candidate));
+
+    description &&
+      description.type !== 'offer' &&
+      (await connection.setRemoteDescription(description));
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 Evt.from<Event>(window, 'resize').attach(() => {
   const { innerWidth, innerHeight } = window;
--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -14,11 +14,86 @@ const { Evt } = evt;
 
 const server = createServer();
 const socketServer = createSocketServer({ server });
-const connections: Map<ws, RTCPeerConnection> = new Map();
+const channels: Map<ws, RTCDataChannel> = new Map();
 
 Evt.from<ws>(socketServer, 'connection').attach((socket) => {
-  connections.set(socket, createPeerConnection());
-  console.log(connections);
+  // TODO: do something with `Evt.newCtx()`
+  const connection = createPeerConnection();
+
+  Evt.from<ws.CloseEvent>(socket, 'close').attachOnce(() => {
+    connection.close();
+    channels.delete(socket);
+  });
+
+  Evt.from<RTCDataChannelEvent>(connection, 'datachannel').attach(
+    ({ channel }) => {
+      channels.set(socket, channel);
+
+      Evt.merge([
+        Evt.from<Event>(channel, 'close'),
+        Evt.from<RTCErrorEvent>(channel, 'error'),
+      ]).attach(() => {
+        socket.close();
+
+        // TODO: this might not be necessary?
+        connection.close();
+        channels.delete(socket);
+      });
+
+      Evt.from<MessageEvent<string>>(channel, 'message').attach(({ data }) => {
+        channels.forEach((peerChannel, peerSocket) => {
+          if (peerSocket === socket) {
+            return;
+          }
+
+          peerChannel.send(data);
+        });
+      });
+    },
+  );
+
+  /**
+   * @see https://developer.mozilla.org/en-US/docs/Web/API/WebRTC_API/Perfect_negotiation#Handling_incoming_ICE_candidates
+   */
+  Evt.from<RTCPeerConnectionIceEvent>(connection, 'icecandidate').attach(
+    ({ candidate }) => {
+      socket.send(JSON.stringify({ candidate }));
+    },
+  );
+
+  /**
+   * @see https://developer.mozilla.org/en-US/docs/Web/API/WebRTC_API/Perfect_negotiation#Handling_incoming_messages_on_the_signaling_channel
+   */
+  Evt.from<ws.MessageEvent>(socket, 'message').attach(async (data) => {
+    if (typeof data !== 'string' || data === 'null') {
+      return;
+    }
+
+    const { candidate, description } = JSON.parse(data);
+    console.log({ candidate, description });
+
+    try {
+      candidate &&
+        candidate.candidate !== '' &&
+        (await connection.addIceCandidate(candidate));
+
+      if (description && description.type === 'offer') {
+        /**
+         * @see https://github.com/node-webrtc/node-webrtc/issues/674
+         * @see https://github.com/node-webrtc/node-webrtc/issues/677
+         */
+        await connection.setRemoteDescription(description);
+        const answer = await connection.createAnswer();
+        await connection.setLocalDescription(answer);
+
+        socket.send(
+          JSON.stringify({ description: connection.localDescription }),
+        );
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  });
 });
 
 server.listen(8080);
