d890e3d0ac1b6222567b1c9c7b2759f4239ba30e
Matt Hayes <matt@mysterycommand.com>
2021-04-10 23:37:55 -0500

    style: add eslint, update prettier

---
+++ packages/eslint-config/README.md
@@ -0,0 +1,11 @@
+# `@rnd/eslint-config`
+
+> TODO: description
+
+## Usage
+
+```
+const eslintConfig = require('@rnd/eslint-config');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/eslint-config/index.js
@@ -0,0 +1,21 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  env: {
+    es2021: true,
+  },
+  rules: {
+    '@typescript-eslint/ban-ts-comment': 'off',
+  },
+  overrides: [
+    {
+      files: ['snowpack.config.cjs'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+  ],
+};
